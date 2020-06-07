813098dba3214646958a897276acd3d0cba416d2
Chris <44944612+chdlsp@users.noreply.github.com>
2020-06-07 22:41:04 +0900

    최초 커밋 - UI 구현

---
+++ src/components/TodoList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+import TodoItem from "./TodoItem";
+
+const TodoListBlock = styled.div`
+  flex: 1;
+  padding: 20px 32px;
+  ppadding-bottom: 48px;
+  overflow-y: auto;
+`;
+
+function TodoList() {
+  return (
+    <TodoListBlock>
+      <TodoItem text="프로젝트 생성하기1" done={true}></TodoItem>
+      <TodoItem text="프로젝트 생성하기2" done={true}></TodoItem>
+      <TodoItem text="프로젝트 생성하기3" done={false}></TodoItem>
+      <TodoItem text="프로젝트 생성하기4" done={false}></TodoItem>
+    </TodoListBlock>
+  );
+}
+export default TodoList;
