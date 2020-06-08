d5552870ef190308bcf268c354d90253bdb36364
Chris <44944612+chdlsp@users.noreply.github.com>
2020-06-08 21:52:52 +0900

    Updates

---
+++ precache-manifest.7c66ae93863ac8957b07fb067ed5b4be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81354e954a30df62cd47ed8b6c97aea1",
+    "url": "/react-tutorial-todoList/index.html"
+  },
+  {
+    "revision": "0d53a8844118afdb399d",
+    "url": "/react-tutorial-todoList/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "acf25dc5a67e18ddc865",
+    "url": "/react-tutorial-todoList/static/js/2.8c3d6214.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-tutorial-todoList/static/js/2.8c3d6214.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d53a8844118afdb399d",
+    "url": "/react-tutorial-todoList/static/js/main.7e6c1781.chunk.js"
+  },
+  {
+    "revision": "ade4fc229d8a6080bbdc",
+    "url": "/react-tutorial-todoList/static/js/runtime-main.191c3261.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-tutorial-todoList/static/media/logo.5d5d9eef.svg"
+  }
+]);
